cc4c9dadebe439123889d2bc0dda4cd818b9b384
Shimmyshanga <luca.junghans@gmail.com>
2017-02-15 10:44:34 -0800

    text in about changed

--- client/controllers/adminController.js
+++ client/controllers/adminController.js
@@ -1,7 +1,6 @@
 app.controller('adminController', ['postFactory', '$location', '$scope', function(postFact, loc, scope){
-    console.log('admincontroller');
-    scope.submit = postFact.newBlogPost;
     if(!scope.luca){
         loc.url('/home');
     }
+    scope.submit = postFact.newBlogPost;
 }]);
